e8494e55dc7cf1e3e9010730f38a5be491e93cb1
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-27 15:59:13 +0530

    [sunil] Add : Implemented Bussiness Layer in Manager

---
+++ Manager/Interfaces/IUserManager.cs
@@ -0,0 +1,18 @@
+﻿using Common.RequestModels;
+using Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Interfaces
+{
+    public interface IUserManager
+    {
+        public User UserRegistration(RegisterModel model);
+        public string UserLogin(LoginModel model);
+        public string ForgetPass(string Email);
+        public bool CheckEmail(string Email);
+        public string GenerateToken(string Email, int Id);
+        public bool ResetPassword(string Email, ResetPasswordModel resetPasswordModel);
+    }
+}
---
+++ Manager/Services/UserManager.cs
@@ -0,0 +1,43 @@
+﻿using Common.RequestModels;
+using Repository.Entity;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Interfaces
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserRepository repository;
+        public UserManager(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+        public User UserRegistration(RegisterModel model)
+        {
+            return repository.UserRegistration(model);
+        }
+        public string UserLogin(LoginModel model)
+        {
+            return repository.UserLogin(model);
+        }
+        public string ForgetPass(string Email)
+        {
+            return repository.ForgetPass(Email);
+        }
+        public bool CheckEmail(string Email)
+        {
+            return repository.CheckEmail(Email);
+        }
+        public bool ResetPassword(string Email, ResetPasswordModel resetPasswordModel)
+        {
+            return  repository.ResetPassword(Email, resetPasswordModel);
+        }
+        public string GenerateToken(string Email, int Id)
+        {
+            return repository.GenerateToken(Email, Id);
+        }
+
+    }
+}
