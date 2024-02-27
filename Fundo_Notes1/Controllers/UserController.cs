f272cdd9595c34713d135df004cc10e277555a6a
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-27 15:57:20 +0530

    [sunil] Add : Implemented Presentation Layer in FundoNotes

---
+++ Fundo_Notes1/Controllers/UserController.cs
@@ -0,0 +1,140 @@
+﻿using Common;
+using Common.RequestModels;
+using Common.ResponseModel;
+using Manager.Interfaces;
+using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Context;
+using Repository.Entity;
+using Repository.Migrations;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fundo_Notes1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserManager userManager;
+        private readonly IBus bus;
+        private readonly DemoContext demoContext;
+        
+
+        public UserController(IUserManager userManager,IBus bus,DemoContext demoContext)
+        {
+            this.userManager = userManager;
+            this.bus = bus;
+            this.demoContext = demoContext;
+        }
+        [HttpPost]
+        [Route("Reg")]
+        public ActionResult Register(RegisterModel model)
+        {
+            try
+            {
+                var response = userManager.UserRegistration(model);
+                if (response != null)
+                {
+                    return Ok(new ResModel<User> { Success = true, Message = "register successfull", Data = response });
+                }
+                else
+                {
+                    return BadRequest(new ResModel<User> { Success = false, Message = "Register failed", Data = response });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResModel<User> { Success = false, Message =ex.Message, Data = null });
+
+            }
+           
+        }
+        [HttpPost]
+        [Route("log")]
+        public ActionResult Login(LoginModel model)
+        {
+            try
+            {
+                string response = userManager.UserLogin(model);
+                if (response != null)
+                {
+                    return Ok(new ResModel<string> { Success = true, Message = "Login successful", Data = response });
+                }
+                else
+                {
+                    return BadRequest(new ResModel<string> { Success = false, Message = "Login Failed", Data = response });
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(new ResModel<string> { Success = false, Message = ex.Message, Data = null });
+
+            }
+        }
+        [HttpPost]
+        [Route("Forgot")]
+        public async Task<IActionResult> ForgotPassword(string Email)
+        {
+            try
+            {
+                if (userManager.CheckEmail(Email))
+                {
+                   Send mail = new Send();
+                   var  forgotPasswordModel = userManager.ForgetPass(Email);
+                   var checkmail=demoContext.UserTable.FirstOrDefault(x => x.Email== Email);
+                    var token = userManager.GenerateToken(checkmail.Email, checkmail.Id);
+                    mail.SendMail(Email,token);
+                    Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                    var endPoint=await bus.GetSendEndpoint(uri);
+                    await endPoint.Send(forgotPasswordModel);
+                    return Ok(new ResModel<string> {Success=true, Message ="Mail sent successfully",Data=token});
+
+                }
+                else
+                {
+                    return BadRequest(new ResModel<string> { Success = false, Message = "Email Does Not Exit", Data = Email });
+
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+           
+        }
+        [Authorize]
+        [HttpPost]
+        [Route("Reset")]
+        public ActionResult ResetPassword(ResetPasswordModel reset)
+        {
+            try
+            {
+                string Email = User.FindFirst("Email").Value;
+                if (userManager.ResetPassword(Email, reset))
+                {
+                    return Ok(new ResModel<bool> { Success = true, Message = "Password Changed", Data = true });
+
+                }
+                else
+                {
+                    return BadRequest(new ResModel<bool> { Success = false, Message = "Password Not Changed", Data = false });
+                }
+
+
+            }
+            catch(Exception ex)
+            { 
+                throw ex; 
+            }
+        }
+
+
+    }
+}
---
+++ Fundo_Notes1/Startup.cs
@@ -0,0 +1,138 @@
+using Manager.Interfaces;
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Repository.Context;
+using Repository.Interfaces;
+using Repository.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundo_Notes1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DemoContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:FundoDb"]));
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IUserManager, UserManager>();
+            services.AddControllers();
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+            });
+            services.AddMassTransit(x =>
+            {
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(config =>
+                {
+                    config.UseHealthCheck(provider);
+                    config.Host(new Uri("rabbitmq://localhost"), h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                }));
+            });
+            services.AddMassTransitHostedService();
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                var Key = Encoding.UTF8.GetBytes(Configuration["JWT:Key"]);
+                o.SaveToken = true;
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["JWT:Issuer"],
+                    ValidAudience = Configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Key)
+                };
+            });
+
+           
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseSwagger();
+
+            // This middleware serves the Swagger documentation UI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+               
+            });
+
+        }
+        
+    }
+}
