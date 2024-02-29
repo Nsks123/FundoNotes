88754b6517fe33a22427c3e9157bd63f55922937
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-29 21:40:35 +0530

    [sunil] Add : Implemented Functionality Notes Table

--- Manager/obj/Debug/netcoreapp3.1/Manager.AssemblyInfo.cs
+++ Manager/obj/Debug/netcoreapp3.1/Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ec131cc4bb83af233d844e9c4ee02357cd86b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
