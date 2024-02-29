4ead0eae89250626089e1e903b07b02648a4ca95
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-29 21:40:14 +0530

    [sunil] Add : Implemented Functionality Notes Table

---
+++ Repository/Migrations/20240228045648_fee.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class fee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FeedBackTable",
+                columns: table => new
+                {
+                    feedback_id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(nullable: false),
+                    feedback_date = table.Column<string>(nullable: true),
+                    feedback_message = table.Column<string>(nullable: true),
+                    rating = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeedBackTable", x => x.feedback_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FeedBackTable");
+        }
+    }
+}
