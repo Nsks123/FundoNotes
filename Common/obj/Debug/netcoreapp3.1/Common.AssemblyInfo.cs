3f67809aac7fa139d55b637dfee9ec64bfff20db
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-29 21:40:49 +0530

    [sunil] Add : Implemented Functionality Notes Table

--- Common/obj/Debug/netcoreapp3.1/Common.AssemblyInfo.cs
+++ Common/obj/Debug/netcoreapp3.1/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ec131cc4bb83af233d844e9c4ee02357cd86b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
