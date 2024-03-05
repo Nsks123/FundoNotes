f5f8f69973e4f884d1562d5f1b535ac4545309a2
Nsks123 <nallamsunilkumar5@gmail.com>
2024-03-05 09:53:19 +0530

    [sunil] Add : Added Functionality To add Label to Notes

---
+++ Common/RequestModels/AddLabel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.RequestModels
+{
+    public class AddLabel
+    {
+     
+        
+        public string LabelName { get; set; }
+
+
+    }
+}
